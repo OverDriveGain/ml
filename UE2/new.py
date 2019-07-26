75eda510cb86c7ffc457721b4ed087f79b3f2d5a
OverDriveGain <manar.zaboub@gmail.com>
2019-07-26 12:06:53 +0200

    Uncompleted solution for ue2 with reference error

--- UE2/new.py
+++ UE2/new.py
@@ -1,41 +1,134 @@
 import pandas as pd
 import numpy as np
 import datetime
+import math
 from operator import add
+
+class gaussianVerteilung:
+    def __init__(self, name, age):
+        self.classes = [[]]
+    def train(self, xTrain, yTrain):
+
+        # Klassen ordnen
+        for index in range(0, len(xTrain)):
+            self.classes[int(yTrain[index])].append(xTrain[index,:])
+
+        # Berechne m fuer die Klassen
+        for num, vectors in enumerate(classes):
+            for vector in vectors:
+                if(len(mues[num]) == 0): mues[num] = vector.copy()     ##Reference by value error!
+                else: mues[num] += vector
+            mues[num] = mues[num] / len(vectors)
+
+        # Berechne kovarianz Matrix fuer die Klassen
+        #calculate covariance matrix
+        sigmas = []
+        for index in range(0, len(classes)):
+            sigma = []
+            for num, vector in enumerate(classes[index]):
+                sub = vector - mues[index]
+                #           if(index==0): print(sub)
+                #            if(index ==0): print(mues[index])
+                #           if(index ==0):
+                #              print (sub)
+                #             print(np.dot(sub[np.newaxis].T , sub[np.newaxis]))
+                if(sigma ==[]):
+                    sigma = np.subtract(vector, mues[index]) * np.subtract(vector, mues[index])[np.newaxis].T
+                else:
+                    sigma += np.subtract(vector, mues[index]) * np.subtract(vector, mues[index])[np.newaxis].T
+                if(index ==0): print ((sigma))
+
+            #        if(index ==0): print (sigma)
+            sigma = sigma / len(classes[index])
+            if(index ==0): print ((sigma))
+    #      if(index !=0): print ((sigma))
+    #     sigmas.append(sigma)>
+
+
 
 testArray = np.array([[5 ,10 ,8], [4,6,3] , [2, 3,3] , [6,12, 3 ] , [8,14,13]])
 
-def readFile(dir):
-    df = pd.read_csv(dir, header=None, sep=" ")
-    X = df.iloc[:,:257].values
-    return X[:10]
+dev = 1
+test = False
+
+valuesCount = -1;
+if(dev):
+    valuesCount = 20
+
+def loadFromFile(path):
+    df = pd.read_csv(path, header=None, sep=" ")
+    X = df.iloc[:valuesCount, 1:257].values # there is an empty string at position 257, because every line ends with a space (== separator)
+    y = df.iloc[:valuesCount, 0].values
+    return X, y
 
 def start():
-    trainVectors = readFile("zip.train/zip.train")
+    xTrain, yTrain = loadFromFile("zip.train/zip.train")
 
     # separate classes.
-    classes = [[]]*10
-    for trainVector in trainVectors:
-        classes[int(trainVector[0])].append(trainVector[1:])
+    classes = np.array([])
+    for i in range(10):
+        np.concatenate(classes,np.array([]))
+    for index in range(0, len(xTrain)):
+        np.concatenate(classes[int(yTrain[index])],xTrain[index,:])
 
-    classes[0] = testArray
-    # calculate sum
-    mues = [0]* 10
-    for index in range (0, len(classes)):
-        mues[index] = np.array([sum(x) for x in zip(*classes[index])])/ len(classes[index])
-    print("Sum of class one vectors is " + str(mues[0]))
+    if(test):
+       classes[0] = testArray
+    print("CLAASSES ARRAY")
+    # calculate mue
+    mues = [np.array([])]* 10
+    for num, vectors in enumerate(classes):
+        for vector in classes[num]:
+            if(len(mues[num]) == 0): mues[num] = vector.copy()     ##Reference by value error!
+            else: mues[num] += vector
+        mues[num] = mues[num] / len(vectors)
+        print(mues[num])
+    #calculate covariance matrix
+    sigmas = []
+    for index in range(0, len(classes)):
+        sigma = []
+        for num, vector in enumerate(classes[index]):
+            sub = vector - mues[index]
+ #           if(index==0): print(sub)
+#            if(index ==0): print(mues[index])
+ #           if(index ==0):
+  #              print (sub)
+   #             print(np.dot(sub[np.newaxis].T , sub[np.newaxis]))
+            if(sigma ==[]):
+                sigma = np.subtract(vector, mues[index]) * np.subtract(vector, mues[index])[np.newaxis].T
+            else:
+                sigma += np.subtract(vector, mues[index]) * np.subtract(vector, mues[index])[np.newaxis].T
 
-    #calculate kovarianz
-    for index in range( 0,len(classes)):
-        substraction = []
-        for index2 in range (0, len(classes[index])):
-            print("Multiplying" + str(np.subtract(classes[index][index2], mues[index])[np.newaxis].T) + " with " + str(np.subtract(classes[index][index2], mues[index])))
-            print("LENGH OF FIRST " + str(len(np.subtract(classes[index][index2], mues[index]))) + " len of second" + str(len(np.subtract(classes[index][index2], mues[index])[np.newaxis].T)))
-            substraction = np.subtract(classes[index][index2], mues[index]) * np.subtract(classes[index][index2], mues[index])[np.newaxis].T
-            print("Substraction" + str(substraction))
+    #        if(index ==0): print (sigma)
+        sigma = sigma / len(classes[index])
+        sigmas.append(sigma)
+  #      if(index !=0): print ((sigma[32:55]))
+  #      if(index !=0): print ((sigma))
+   #     sigmas.append(sigma)
 
-    substraction = substraction / len(classes[index])
 
+    xTest, yTest = loadFromFile("zip.test/zip.test")
+
+    #Prediction phase
+    for index in range (0, len(xTest)):
+        testProbabilities = []
+        for index2 in range(0, len(sigmas)):
+            sub = xTest[index] - mues[index2]
+            sub = np.array(sub)
+            print("Determinant")
+            print(sub)
+            print("forst")
+            print(xTest[index])
+            print("sec")
+            print( mues[index2])
+
+# print(sigmas[index2])
+            probability =  np.exp(-0.5 * sub[np.newaxis].dot(sub[np.newaxis].T)) / np.sqrt( abs(2*math.pi*np.linalg.det(sigmas[index2])))   #Transpose at end: just simple way to reverse the attributes to dimensions of each vector
+            #print(probability)
+#            probability =  np.linalg.det(sigmas[index2])
+            if(probability !=0 or True):
+                print("Klasse is" + str(index2) + " that "+ str(probability))
+            testProbabilities.append(probability)
+    print("Finished")
 
 
 
